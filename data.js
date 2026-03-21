3564e39a167435ef37101e8cba9a9a94990df46a
jergan-studio <jose343lopez22@gmail.com>
2026-03-21 12:36:35 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const database = [
+  {
+    title: "YouTube",
+    url: "https://youtube.com",
+    description: "Watch videos online"
+  },
+  {
+    title: "GitHub",
+    url: "https://github.com",
+    description: "Code hosting platform"
+  },
+  {
+    title: "Jref API",
+    url: "#",
+    description: "Your custom API tools"
+  }
+];
