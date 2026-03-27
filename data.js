c8f80e8bbe1e4f4a5c4bdb2fce81c4ed9bcef12d
jergan-studio <jose343lopez22@gmail.com>
2026-03-27 17:27:38 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,4 @@
 const database = [
-  {
-    title: "YouTube",
-    url: "https://youtube.com",
-    description: "Watch videos online"
-  },
-  {
-    title: "GitHub",
-    url: "https://github.com",
-    description: "Code hosting platform"
-  },
-  {
-    title: "Jref API",
-    url: "#",
-    description: "Your custom API tools"
-  }
+  { title:"YouTube", url:"https://youtube.com", description:"Videos" },
+  { title:"GitHub", url:"https://github.com", description:"Code" }
 ];
